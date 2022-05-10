b4c4028834d719f743311dc741a35129cfc1d4b5
Erick Simas Grilo <simasgrilo86@gmail.com>
2022-05-10 16:20:07 -0300

    Working version. Uses IoT wrapper from SAP

--- MQTT.py
+++ MQTT.py
@@ -1,56 +1,76 @@
-import certifi
-import paho.mqtt.publish as mqttPublish
+
 import paho.mqtt.client as mqttClient
 import random
+import iot_services_sdk.mqtt_client as MQTTSAP
 import ssl
 import time
 
-class MqttSap():
+class MqttSap(MQTTSAP.PahoMQTT):
 
-    def __init__(self,clientID,certificateFile):
+    def __init__(self,clientID,certificateFile,secret):
+
         self.serverURL = '29cf1730-34da-4b38-9edd-30be889be55f.eu10.cp.iot.sap'
         self.port = 8883
         self.clientID = clientID
         self.topic = "measures/" + clientID
         self.topicSubscribe = "ack/" + clientID
         self.certificateFile = certificateFile
+        self.keyStore = "./certificates/client.ks"
+        self.secret = secret
 
     def onConnect(self,client: mqttClient,uesrdata,flags,rc):
-        print("Conectado: código de retorno = " + str(rc))
-        client.subscribe(topic=self.topicSubscribe)
+        print("Connected: Return code = " + str(rc))
+
+    def onMessage(self,userdata,message):
+        print("Message received in main topic:")
+        print(message)
+
+    def onCommand(self,client,userdata,message):
+        print("Broker:")
+        print(message)
+
+    def onError(self,userdata,report,a):
+        print("Error message: ")
+        print(report)
 
     def connect(self):
-        client = mqttClient.Client(client_id=self.clientID)
+        client = MQTTSAP.MQTTClient(device_alternate_id=self.clientID,
+                                    instance=self.serverURL,
+                                    secret=self.secret,
+                                    pemfile=self.certificateFile)
         client.on_connect = self.onConnect
-        #print(certifi.where())
-        client.tls_set(
-                       certfile=self.certificateFile,
-                       cert_reqs=ssl.CERT_REQUIRED, tls_version=ssl.PROTOCOL_TLS, ciphers=None
-        )
-        client.connect(host=self.serverURL, port=self.port, keepalive=60)
-        client.subscribe(topic=self.topicSubscribe)
+        client.on_command = self.onCommand
+        client.on_message = self.onMessage
+        client.on_error = self.onError
+
+        try:
+            client.tls_set(pemfile=self.certificateFile,secret=self.secret)
+        except ValueError:
+            print("TLS already set for this connection")
+        client.connect()
+        client.subscribe(device_alternate_id=self.clientID)
         return client
 
-    def publish(self,client):
+
+    def publish(self,client,capAltId,SensorAltId):
         while True:
-            message = str({"capabilityAlternateId": "[BRA] Air conditioning",
-               "sensorAlternateId": "BRA_Air_conditioning",
-               "measures": [[random.randint(1, 60)]]
-               })
-            time.sleep(3000)
-            result = client.publish(self.topic, message)
+            time.sleep(10)
+            result = client.publish(capability_alternate_id="[BRA] Air conditioning",
+                                    sensor_alternate_id="BRA_Air_conditioning",
+                                    measures=[[random.randint(1,60)]])
             print(result)
             status = result[0]
             if status == 0:
-                print(f"Send '{message}' to topic `{self.topic}`")
+                print(f"Send 'message' to topic `{self.topic}`")
             else:
                 print(f"Failed to send message to topic {self.topic}")
 
+
     def runMQTT(self):
         client = self.connect()
-        #client.loop_forever()
         client.loop_start()
-        self.publish(client)
-
+        for _ in range(0,10):
+            self.publish(client,"BRA_Air_conditioning","[BRA] Air conditioning")
+        client.loop_stop()
 
 
--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from MQTT import MqttSap
-#import test
 
+#from test import MqttSap
+
+import iot_services_sdk.mqtt_client as mqttSAP
+import random
 
 if __name__ == '__main__':
-    conn = MqttSap("68FFC2DD2E344280885511AD07E61A8",
-                   "./certificates/certificate_real.pem")
+    conn = MqttSap("50B0D0984B17407688F9714CFAFBA6DA",
+                   "./certificates/certificate.pem",
+                   "zFYhclwBxX#4ntETy#cZA7Lu0r38gWm6!21l")
+
     conn.runMQTT()
     #test.run()
+
+
 
